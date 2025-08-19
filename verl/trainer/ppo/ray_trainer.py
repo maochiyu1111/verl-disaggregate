bugfix for gradients transfer, have trouble in backword

verl/trainer/ppo/ray_trainer.py
@@ -1332,6 +1332,7 @@ class RayPPOTrainer:
                     from numpy.dtypes import ObjectDType
                     torch.serialization.add_safe_globals([DataProto, _reconstruct, ndarray, numpy.dtype, ObjectDType])
                     batch = torch.load("update_batch.pt", map_location="cpu")
+                    batch = batch.split(96)[0]
                     # breakpoint()
                     # implement critic warmup
                     if self.config.trainer.critic_warmup <= self.global_steps:
@@ -1340,8 +1341,9 @@ class RayPPOTrainer:
                             batch.meta_info["multi_turn"] = self.config.actor_rollout_ref.rollout.multi_turn.enable
                             # actor_output = self.actor_rollout_wg.update_actor(batch)
                             encoder_embed = self.actor_rollout_encoder_wg.actor_forward(batch)
-                            actor_output, encoder_gradients = self.actor_rollout_llm_wg.update_actor(batch, encoder_embed)
-                            self.actor_rollout_encoder_wg.update_actor(encoder_gradients)
+                            # batch = batch.union_non_tensor(encoder_embed)
+                            actor_output = self.actor_rollout_llm_wg.update_actor(batch, encoder_embed)
+                            self.actor_rollout_encoder_wg.update_actor(actor_output)
                         actor_output_metrics = reduce_metrics(actor_output.meta_info["metrics"])
                         metrics.update(actor_output_metrics)
 

verl/workers/actor/dp_actor.py
@@ -112,9 +112,13 @@ class DataParallelPPOActor(BasePPOActor):
             batch_size, seqlen = input_ids.shape
             attention_mask = micro_batch["attention_mask"]
             position_ids = micro_batch["position_ids"]
-            image_embed = micro_batch["image_embed"] if "image_embed" in micro_batch else None
-            video_embed = micro_batch["video_embed"] if "video_embed" in micro_batch else None 
+            image_embed = micro_batch["image_embed"][0] if "image_embed" in micro_batch else None
+            video_embed = micro_batch["video_embed"][0] if "video_embed" in micro_batch else None 
             if encoder_grad:
+                if image_embed is not None:
+                    image_embed.requires_grad_(True)
+                if video_embed is not None:
+                    video_embed.requires_grad_(True)
                 self.current_image_embed = image_embed
                 self.current_video_embed = video_embed
             entropy = None
@@ -574,7 +578,7 @@ class DataParallelPPOActor(BasePPOActor):
 
 
     @GPUMemoryLogger(role="dp actor", logger=logger)
-    def update_policy_llm(self, data: DataProto, encoder_embed: dict):
+    def update_policy_llm(self, data: DataProto, encoder_embed: DataProto):
         # make sure we are in training mode
         self.actor_module.train()
 
@@ -600,13 +604,13 @@ class DataParallelPPOActor(BasePPOActor):
         # Split to make minibatch iterator for updating the actor
         # See PPO paper for details. https://arxiv.org/abs/1707.06347
         mini_batches = data.split(self.config.ppo_mini_batch_size)
-        mini_batches_encoder_embed = dict_split(encoder_embed, self.config.ppo_mini_batch_size) 
+        mini_batches_embed = encoder_embed.split(self.config.ppo_mini_batch_size // self.config.ppo_micro_batch_size_per_gpu)
 
         metrics = {}
         image_embed_grad_list = []
         video_embed_grad_list = []
         for _ in range(self.config.ppo_epochs):
-            for batch_idx, mini_batch in enumerate(mini_batches):
+            for mini_batch, mini_batch_embed in zip(mini_batches, mini_batches_embed):
                 if self.config.use_dynamic_bsz:
                     max_token_len = self.config.ppo_max_token_len_per_gpu * self.ulysses_sequence_parallel_size
                     micro_batches, _ = prepare_dynamic_batch(mini_batch, max_token_len=max_token_len)
@@ -615,14 +619,13 @@ class DataParallelPPOActor(BasePPOActor):
                         self.config.ppo_mini_batch_size // self.config.ppo_micro_batch_size_per_gpu
                     )
                     micro_batches = mini_batch.split(self.config.ppo_micro_batch_size_per_gpu)
-                    micro_batches_encoder_embed = dict_split(mini_batches_encoder_embed, self.config.ppo_micro_batch_size_per_gpu)
+                    micro_batches_embed = mini_batch_embed.split(1)
 
                 self.actor_optimizer.zero_grad()
 
-                for micro_batch, encoder_embed in zip(micro_batches, micro_batches_encoder_embed):
+                for micro_batch, micro_batch_embed in zip(micro_batches, micro_batches_embed):
                     micro_batch_metrics = {}
-                    breakpoint()
-                    model_inputs = {**micro_batch.batch, **micro_batch.non_tensor_batch, **encoder_embed}
+                    model_inputs = {**micro_batch.batch, **micro_batch.non_tensor_batch, **micro_batch_embed.non_tensor_batch}
                     response_mask = model_inputs["response_mask"]
                     old_log_prob = model_inputs["old_log_probs"]
                     advantages = model_inputs["advantages"]
@@ -634,9 +637,10 @@ class DataParallelPPOActor(BasePPOActor):
                     calculate_entropy = False
                     if entropy_coeff != 0:
                         calculate_entropy = True
-                    entropy, log_prob = self._forward_micro_batch(
-                        model_inputs, temperature=temperature, calculate_entropy=calculate_entropy, disaggregate=True, encoder_grad=True
-                    )
+                    with torch.enable_grad():
+                        entropy, log_prob = self._forward_micro_batch(
+                            model_inputs, temperature=temperature, calculate_entropy=calculate_entropy, disaggregate=True, encoder_grad=True
+                        )
 
                     loss_mode = self.config.policy_loss.get("loss_mode", "vanilla")
                     # vanilla -> verl.trainer.ppo.core_algos.compute_policy_loss_vanilla
@@ -679,10 +683,10 @@ class DataParallelPPOActor(BasePPOActor):
                         loss = policy_loss / self.gradient_accumulation
                     loss.backward()
 
-                    if hasattr(self, 'current_image_embed'):
+                    if hasattr(self, 'current_image_embed') and self.current_image_embed is not None:
                         image_embed_grad = self.current_image_embed.grad
                         image_embed_grad_list.append(image_embed_grad)
-                    if hasattr(self, 'current_video_embed'):
+                    if hasattr(self, 'current_video_embed') and self.current_video_embed is not None: 
                         video_embed_grad = self.current_video_embed.grad
                         video_embed_grad_list.append(video_embed_grad)
                     
@@ -696,16 +700,24 @@ class DataParallelPPOActor(BasePPOActor):
                     )
                     append_to_dict(metrics, micro_batch_metrics)
 
-                grad_norm = self._optimizer_step()
-                mini_batch_metrics = {"actor/grad_norm": grad_norm.detach().item()}
-                append_to_dict(metrics, mini_batch_metrics)
-    
-        if image_embed_grad_list[0] is not None and video_embed_grad_list[0] is not None:
-            encoder_gradients = {"image_embed_grad_list": image_embed_grad_list, "video_embed_grad_list": video_embed_grad_list}
-        elif image_embed_grad_list[0]  is not None and video_embed_grad_list[0] is None:
-            encoder_gradients = {"image_embed_grad_list": image_embed_grad_list}
-        elif image_embed_grad_list[0] is None and video_embed_grad_list[0] is not None:
-            encoder_gradients = {"video_embed_grad_list": video_embed_grad_list}
+                # grad_norm = self._optimizer_step()
+                # mini_batch_metrics = {"actor/grad_norm": grad_norm.detach().item()}
+                # append_to_dict(metrics, mini_batch_metrics)
+                
+        def flatten_embeds(embeds_list: list):
+            if not embeds_list :
+                return []
+            return [tensor.cpu().to(dtype=torch.float32) for tensor in embeds_list]
+        
+        image_embed_grad_list = flatten_embeds(image_embed_grad_list)
+        video_embed_grad_list = flatten_embeds(video_embed_grad_list)
+        
+        if image_embed_grad_list and video_embed_grad_list:
+            encoder_gradients = {"image_embed_grad": image_embed_grad_list, "video_embed_grad": video_embed_grad_list}
+        elif image_embed_grad_list and not video_embed_grad_list:
+            encoder_gradients = {"image_embed_grad": image_embed_grad_list}
+        elif not image_embed_grad_list and video_embed_grad_list:
+            encoder_gradients = {"video_embed_grad": video_embed_grad_list}
         else:
             raise ValueError("Both image_embed and video_embed are None. At least one of them must be provided.")
         self.actor_optimizer.zero_grad()

verl/workers/encoder/dp_encoder.py
@@ -179,32 +179,24 @@ class DataParallelPPOEncoder(BasePPOEncoder):
                 self.image_embeds_list.append(image_embeds)
                 self.video_embeds_list.append(video_embeds)
 
-        # def flatten_embeds(embeds_list: list):
-        #     # 可能其中一个是None列表
-        #     if not embeds_list or embeds_list[0] is None:
-        #         return None
-        #     return [tensor.cpu() for tensor_tuple in embeds_list for tensor in tensor_tuple]
+        def flatten_embeds(embeds_list: list):
+            # 可能其中一个是None列表
+            if not embeds_list or embeds_list[0] is None:
+                return None
+            return [tensor.detach().cpu().to(dtype=torch.float32) for tensor in embeds_list]
         
             
-        # image_embeds = flatten_embeds(self.image_embeds_list)
-        # video_embeds = flatten_embeds(self.video_embeds_list)
-            
-        return {"image_embed": self.image_embeds_list, "video_embed": self.video_embeds_list}
+        image_embeds = flatten_embeds(self.image_embeds_list)
+        video_embeds = flatten_embeds(self.video_embeds_list)
+        return image_embeds, video_embeds
 
     @GPUMemoryLogger(role="dp actor", logger=logger)
-    def update_policy(self, data: dict):
+    def update_policy(self, data: DataProto):
         # make sure we are in training mode
         # 想方法拿到每个microbatch的gradient
         self.encoder_module.train()
         
-        mini_batch_size = self.config.ppo_mini_batch_size
-        mini_batches = []
-        # mini_batches = data.split(self.config.ppo_mini_batch_size)
-        total_length = len(next(iter(data.values())))
-        for i in range(0, total_length, mini_batch_size):
-            mini_batch = {key: values[i : i + mini_batch_size] for key, values in data.items()}
-            mini_batches.append(mini_batch)
-            
+        mini_batches = data.split(self.config.ppo_mini_batch_size // self.config.ppo_micro_batch_size_per_gpu)    
         # metrics = {}
         for _ in range(self.config.ppo_epochs):
             global_index = 0
@@ -213,30 +205,33 @@ class DataParallelPPOEncoder(BasePPOEncoder):
                 self.gradient_accumulation = (
                     self.config.ppo_mini_batch_size // self.config.ppo_micro_batch_size_per_gpu
                 )
-                # micro_batches = mini_batch.split(self.config.ppo_micro_batch_size_per_gpu)
-                micro_batch_size = self.config.ppo_micro_batch_size_per_gpu
-                micro_batches = []
-                mini_total_length = len(next(iter(mini_batch.values())))
-                for i in range(0, mini_total_length, micro_batch_size):
-                    micro_batch = {key: values[i : i + micro_batch_size] for key, values in mini_batch.items()}
-                    micro_batches.append(micro_batch)
-
+                micro_batches = mini_batch.split(1)
                 self.encoder_optimizer.zero_grad()
 
+                breakpoint()
+                success_count = 0
                 for micro_batch in micro_batches:
                     current_image_embed = self.image_embeds_list[global_index]
                     current_video_embed = self.video_embeds_list[global_index]
-                    image_grad = micro_batch.get("image_embed_grad_list", None)
-                    video_grad = micro_batch.get("video_embed_grad_list", None)
-                    breakpoint()
+                    image_grad = micro_batch.non_tensor_batch["image_embed_grad"][0].to(dtype=torch.bfloat16, device=torch.cuda.current_device()) if "image_embed_grad" in micro_batch.non_tensor_batch.keys() else None
+                    video_grad = micro_batch.non_tensor_batch["video_embed_grad"][0].to(dtype=torch.bfloat16, device=torch.cuda.current_device()) if "video_embed_grad" in micro_batch.non_tensor_batch.keys() else None
                     if current_image_embed is not None and image_grad is not None:
-                        assert current_image_embed.shape == image_grad.shape
-                        current_image_embed.backward(gradient=image_grad, retain_graph=True)
+                        if global_index == 1:
+                            global_index += 1
+                            continue
+                        try:
+                            assert current_image_embed.shape == image_grad.shape
+                            current_image_embed.backward(gradient=image_grad, retain_graph=True)
+                            success_count += 1
+                        except Exception:
+                            pass
                     if current_video_embed is not None and video_grad is not None:
                         assert current_video_embed.shape == video_grad.shape
                         current_video_embed.backward(gradient=video_grad)
                     global_index += 1
-
+                    print("global:",global_index)
+                    print("success:", success_count)
+                breakpoint()
                 grad_norm = self._optimizer_step()
                 # 先不传入这部分的gradient
                 # mini_batch_metrics = {"actor/grad_norm": grad_norm.detach().item()}

verl/workers/fsdp_workers.py
@@ -1400,8 +1400,9 @@ class ActorRolloutRefWorker_encoder(Worker):
             )
 
     @register(dispatch_mode=Dispatch.DP_COMPUTE_PROTO)
-    def update_actor(self, data: dict):
+    def update_actor(self, data: DataProto):
         # Support all hardwares
+        data = DataProto(non_tensor_batch=data.non_tensor_batch)
         data = data.to(torch.cuda.current_device())
 
         assert self._is_actor
@@ -1449,8 +1450,17 @@ class ActorRolloutRefWorker_encoder(Worker):
         if self._is_offload_param:
             load_fsdp_model_to_gpu(self.actor_module_fsdp)  
         data = data.to(torch.cuda.current_device())     
-        encoder_embed = self.actor.extract_feature_train(data=data)    
-        output = dict_to(encoder_embed, "cpu")
+        image_embed, video_embed = self.actor.extract_feature_train(data=data)    
+        if image_embed is not None and video_embed is not None:
+                embeds = {"image_embed": image_embed, "video_embed": video_embed}
+        elif image_embed is not None and video_embed is None:
+            embeds = {"image_embed": image_embed}
+        elif image_embed is None and video_embed is not None:
+            embeds = {"video_embed": video_embed}
+        else:
+            raise ValueError("Both image_embed and video_embed are None. At least one of them must be provided.")
+        output = DataProto.from_dict(non_tensors=embeds)
+        output = output.to("cpu")
         
         if self.world_size > 1 and fsdp_version(self.actor.encoder_module) == 1:
             self.actor.encoder_module._handle.reshard(True)
@@ -1706,6 +1716,11 @@ class ActorRolloutRefWorker_llm(Worker):
             )
             # 遇到报错，actor_module在meta，fsdp在cuda:0
             actor_module = actor_module.to_empty(device=torch.device("cuda", torch.cuda.current_device()), recurse=True)
+            lm_head_sd = torch.load("/workspace/models/qwen2.5vl-lm_head.pt")
+            llm_sd = torch.load("/workspace/models/qwen2.5vl-3b-llm.pt")
+            actor_module.lm_head.load_state_dict(lm_head_sd)
+            actor_module.language_model.load_state_dict(llm_sd)
+            # breakpoint()
 
             if use_remove_padding or self.ulysses_sequence_parallel_size > 1:
                 from verl.models.transformers.monkey_patch import apply_monkey_patch
@@ -2033,7 +2048,7 @@ class ActorRolloutRefWorker_llm(Worker):
             )
 
     @register(dispatch_mode=Dispatch.DP_COMPUTE_PROTO)
-    def update_actor(self, data: DataProto, encoder_embed: dict):
+    def update_actor(self, data: DataProto, encoder_embed: DataProto):
         # Support all hardwares
         data = data.to(torch.cuda.current_device())
         encoder_embed = encoder_embed.to(torch.cuda.current_device())
@@ -2062,11 +2077,10 @@ class ActorRolloutRefWorker_llm(Worker):
             metrics["actor/lr"] = lr
 
             # TODO: here, we should return all metrics
-            output = DataProto(meta_info={"metrics": metrics})
-
+            # output = DataProto(meta_info={"metrics": metrics})
+            output = DataProto.from_dict(meta_info={"metrics": metrics}, non_tensors=encoder_gradients)
             output = self.ulysses_sharding_manager.postprocess_data(data=output)
             output = output.to("cpu")
-            encoder_gradients = encoder_gradients.to("cpu")
 
         if self._is_offload_param:
             offload_fsdp_model_to_cpu(self.actor_module_fsdp)
@@ -2075,7 +2089,7 @@ class ActorRolloutRefWorker_llm(Worker):
             offload_fsdp_optimizer(optimizer=self.actor_optimizer)
             log_gpu_memory_usage("After offload actor optimizer during update_actor", logger=logger)
 
-        return output, encoder_gradients
+        return output
 
     @register(dispatch_mode=Dispatch.DP_COMPUTE_PROTO)
     def generate_sequences(self, prompts: DataProto):